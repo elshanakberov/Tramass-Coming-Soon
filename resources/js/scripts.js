4d181fdf9393714562c76ee2fcb0ea7649a13085
elshanakberov <akberov@yahoo.com>
2016-11-15 00:17:00 +0400

    animations added

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -36,5 +36,27 @@ $(document).ready(function(){
             }
           });
 });
+    
+    <!-- Animation -->
+        $('.js--wp-1').waypoint(function(){
+            $('.js--wp-1').addClass('animated fadeIn');
+        }, {
+            offset:'50%'
+        });
+    $('.js--wp-2').waypoint(function(){
+            $('.js--wp-2').addClass('animated fadeInUp');
+        }, {
+            offset:'50%'
+        });
+    $('.js--wp-3').waypoint(function(){
+            $('.js--wp-3').addClass('animated fadeIn');
+        }, {
+            offset:'50%'
+        });
+    $('.js--wp-4').waypoint(function(){
+            $('.js--wp-4').addClass('animated pulse');
+        }, {
+            offset:'50%'
+        });
 });
 
